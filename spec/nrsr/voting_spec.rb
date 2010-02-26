abf2da4c0ea9e4569281df8b6acb58b92a2bc594
Jan Suchal <johno@jsmf.net>
2010-02-26 19:32:57 +0100

    Initial import.

---
+++ spec/nrsr/voting_spec.rb
@@ -0,0 +1,53 @@
+require '../../wrappers/nrsr/voting'
+
+describe NRSR::Voting do
+  it "should parse voting info" do
+    voting = NRSR::Voting.parse(File.read("fixtures/voting1.html"))
+    voting.subject.should == "Hlasovanie o pozmeňujúcich a doplňujúcich návrhoch k programu 48. schôdze Národnej rady Slovenskej republiky.\nPodpr. Belousovová, 1. návrh."
+    voting.meeting_number.should == 48
+    voting.number.should == 3
+    voting.happened_at.should == DateTime.civil(2010, 2, 2, 13, 18)
+  end
+
+  it "should parse voting statistics" do
+    voting = NRSR::Voting.parse(File.read("fixtures/voting1.html"))
+    voting.attending_count.should == 108
+    voting.voting_count.should == 96
+    voting.pro_count.should == 44
+    voting.against_count.should == 0
+    voting.hold_count.should == 52
+    voting.not_voting_count.should == 12
+    voting.not_attending_count.should == 42
+  end
+
+  it "should parse all votes with parties" do
+    voting = NRSR::Voting.parse(File.read("fixtures/voting1.html"))
+    voting.votes["Klub KDH"].should == [
+            ["N", "Abrhan, Pavol"], ["?", "Brocka, Július"], ["?", "Fronc, Martin"], ["?", "Gibalová, Monika"],
+            ["?", "Hrušovský, Pavol"], ["N", "Kahanec, Stanislav"], ["0", "Lipšic, Daniel"], ["?", "Sabolová, Mária"],
+            ["0", "Šimko, Jozef"],
+    ]
+    voting.votes["Klub ĽS – HZDS"].size.should == 15
+    voting.votes["Klub SDKÚ – DS"].size.should == 28
+    voting.votes["Klub SMER – SD"].size.should == 50
+    voting.votes["Klub SMK – MKP"].size.should == 15
+    voting.votes["Klub SNS"].size.should == 19
+    voting.votes["Poslanci, ktorí nie sú členmi poslaneckých klubov"].size.should == 14
+    voting.votes["Poslanci, ktorí nie sú členmi poslaneckých klubov"].last.should == ["?", "Simon, Zsolt"]
+  end
+
+  it "should parse info from secret voting" do
+    voting = NRSR::Voting.parse(File.read("fixtures/voting2.html"))
+    voting.meeting_number.should == 4
+    voting.number.should == nil
+    voting.happened_at.should == DateTime.civil(2006, 9, 5)
+    voting.attending_count.should == 99
+    voting.voting_count.should == 96
+    voting.pro_count.should == 78
+    voting.against_count.should == 13
+    voting.hold_count.should == 5
+    voting.not_voting_count.should == 3
+    voting.not_attending_count.should == 51
+    voting.votes.should == {}
+  end
+end
---
+++ wrappers/nrsr/voting.rb
@@ -0,0 +1,48 @@
+require "nokogiri"
+
+module NRSR
+  class Voting
+    attr_accessor :subject, :meeting_number, :number, :happened_at, :attending_count, :voting_count, :pro_count, :against_count, :hold_count, :not_voting_count, :not_attending_count, :votes
+
+    def self.parse(html)
+      doc = Nokogiri::HTML(html)
+      voting = new
+      voting.subject = doc.css("#ctl15__hlasHeader__nazovLabel").first.content
+      voting.meeting_number = doc.css("#ctl15__hlasHeader__schodzaLink").first.content.match(/\d+/)[0].to_i
+      number_raw = doc.css("#ctl15__hlasHeader__cisHlasovaniaLabel").first.content
+      voting.number = number_raw.match(/\d+/)[0].to_i unless number_raw.empty?
+      time = doc.css("#ctl15__hlasHeader__dateLabel").first.content.match(/(\d+)\. (\d+)\. (\d+) (\d+):(\d+)/)
+      voting.happened_at = DateTime.civil(time[3].to_i, time[2].to_i, time[1].to_i, time[4].to_i, time[5].to_i)
+
+      voting.attending_count = doc.css("#ctl15__hlasHeader__headerCounterPritomni").first.content.to_i
+      voting.voting_count = doc.css("#ctl15__hlasHeader__headerCounterHlasujucich").first.content.to_i
+      voting.pro_count = doc.css("#ctl15__hlasHeader__headerCounterZa").first.content.to_i
+      voting.against_count = doc.css("#ctl15__hlasHeader__headerCounterProti").first.content.to_i
+      voting.hold_count = doc.css("#ctl15__hlasHeader__headerCounterZdrzaloSa").first.content.to_i
+      voting.not_voting_count = doc.css("#ctl15__hlasHeader__headerCounterNehlasovalo").first.content.to_i
+      voting.not_attending_count = doc.css("#ctl15__hlasHeader__headerCounterNepritomni").first.content.to_i
+      voting.votes = parse_votes(doc)
+      voting
+    end
+
+    private
+    def self.parse_votes(doc)
+      votes = {}
+      party = nil
+      doc.css("#ctl15__resultsTable tr td").each do |row|
+        if row["class"] == "h3"
+          party = row.content
+        else
+          spans = row.css("span")
+          unless spans.empty?
+            vote = spans.first.content.tr("[] ", "")
+            person = row.css("a").first.content
+            votes[party] ||= []
+            votes[party] << [vote, person]
+          end
+        end
+      end
+      votes
+    end
+  end
+end
